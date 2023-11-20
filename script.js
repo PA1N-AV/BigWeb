aa1983bc3a53f0a3b58d5186a386cfb391c60f25
Andew <ak5187972@gmail.com>
2023-11-20 21:21:36 +0200

    UI

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 let btn = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
-let searchBtn = document.querySelector('.bxs-color');
-let listItem = document.querySelectorAll('.list-item');
 
 btn.onclick = function(){
-    sidebar.classList.toggle('active');
-}
-
-searchBtn.onclick = function(){
     sidebar.classList.toggle('active');
 }
 
